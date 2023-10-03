be6df35349adba97afed802bab22c0e2324136a0
Ayoub-Haj-Husein <ayoubabdulrahmanhusein@gmail.com>
2023-10-03 16:12:36 +0300

    Add description for chalenge

--- 7kyu/Simple beads count.js
+++ 7kyu/Simple beads count.js
@@ -5,7 +5,7 @@ DESCRIPTION:
 Two red beads are placed between every two blue beads. 
 There are N blue beads. After looking at 
 the arrangement below work out the number of red beads.
-@ @@ @ @@ @ @@ @ @@ @ @@ @
+@ => blue @@=> 2 red @ => blue @@=> 2 red @=> blue @@=> 2 red @=> blue @@=> 2 red @=> blue @@=> 2 red @=> blue
 Implement count_red_beads(n) (in PHP count_red_beads($n); in Java, Javascript,
 TypeScript, C, C++ countRedBeads(n)) so that it returns the number of red beads.
 If there are less than 2 blue beads return 0.
